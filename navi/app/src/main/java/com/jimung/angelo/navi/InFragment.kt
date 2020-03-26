c743391e28b74973ca1984ba9e8c48abe7eda2b8
githubangelopurnama <angelojimung04@gmail.com>
2020-03-26 14:32:25 +0700

    uts proyek

---
+++ navi/app/src/main/java/com/jimung/angelo/navi/InFragment.kt
@@ -0,0 +1,42 @@
+package com.jimung.angelo.navi
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_in.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class InFragment : Fragment() {
+    lateinit var nav : NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_in, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nav = Navigation.findNavController(view)
+        buttonSubmit.setOnClickListener{
+            if (editText.text.toString().isNotEmpty()) {
+                val input = editText.text.toString()
+                val bundle = Bundle()
+                bundle.putString("args", input)
+
+                nav.navigate(R.id.action_inFragment_to_outFragment, bundle)
+            }
+        }
+    }
+
+
+}
